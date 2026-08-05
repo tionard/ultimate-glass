f9693b39b66a3071a69d205b4d196a6c518679ad
tionard <42518379+tionard@users.noreply.github.com>
2026-08-05 15:07:12 +0200

    Initialize blocks, items, and interactions

--- src/main/java/com/github/tionard/ultimateglass/UltimateGlass.java
+++ src/main/java/com/github/tionard/ultimateglass/UltimateGlass.java
@@ -4,12 +4,19 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.github.tionard.ultimateglass.interaction.UltimateGlassInteractions;
+import com.github.tionard.ultimateglass.registry.UltimateGlassBlocks;
+import com.github.tionard.ultimateglass.registry.UltimateGlassItems;
+
 public final class UltimateGlass implements ModInitializer {
     public static final String MOD_ID = "ultimateglass";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing Ultimate Glass");
+        UltimateGlassBlocks.initialize();
+        UltimateGlassItems.initialize();
+        UltimateGlassInteractions.initialize();
+        LOGGER.info("Initialized Ultimate Glass");
     }
 }
