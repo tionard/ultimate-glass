c5c2f1d5b14211f310324882240007a9d555aa19
tionard <42518379+tionard@users.noreply.github.com>
2026-08-05 14:48:36 +0200

    Add client mod initializer

---
+++ src/client/java/com/github/tionard/ultimateglass/client/UltimateGlassClient.java
@@ -0,0 +1,10 @@
+package com.github.tionard.ultimateglass.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class UltimateGlassClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-only registration such as keybinds, previews, and Mod Menu integration goes here.
+    }
+}
